5ba7595cbefd32d1e2ab65253f4937e547a14f8a
J. Langley <jameselangley@gmail.com>
2025-03-04 23:14:29 -0600

    Added visit_webpage

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import pytz
 import yaml
 from tools.final_answer import FinalAnswerTool
+from tools.visit_webpage import VisitWebpageTool
 
 from Gradio_UI import GradioUI
 
@@ -38,6 +39,7 @@ def get_current_time_in_timezone(timezone: str) -> str:
 
 
 final_answer = FinalAnswerTool()
+visit_webpage = VisitWebpageTool()
 
 # If the agent does not answer, the model is overloaded, please use another model or the following Hugging Face Endpoint that also contains qwen2.5 coder:
 # model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
@@ -58,7 +60,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer, get_current_time_in_timezone, my_custom_tool], ## add your tools here (don't remove final answer)
+    tools=[final_answer, get_current_time_in_timezone, my_custom_tool, visit_webpage], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
